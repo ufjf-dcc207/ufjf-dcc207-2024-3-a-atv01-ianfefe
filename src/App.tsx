aaa0e9d7486f3baa206f423debc65092e9c24e89
ianfefe <felix.ian@estudante.ufjf.br>
2024-11-11 20:56:38 -0300

    adiciona disciplinas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ export default function App() {
   return (
     <>
       <h1>Olá mundo!</h1>
+      <Disciplina codigo="DCC202" nome="Desenvolvimento Web I"/>
+      <Disciplina codigo="DCC206" nome="Desenvolvimento Web II"/>
       <Disciplina codigo="DCC207" nome="Desenvolvimento Web Front-End"/>
     </>
   );
